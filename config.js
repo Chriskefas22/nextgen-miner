762e8cb09abf1a807028c6320461c32c21d01ef2
Chriskefas22 <angellhinoc@gmail.com>
2026-08-30 02:43:26 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-export const CONFIG = {
-  SUPABASE_URL: 'https://mmqprhuvhghyuvudsyma.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_T5VIaE38Qx8VmzBOJQSODg_b_2WfWu6',
-  TURNSTILE_SITE_KEY: '',
-  CURRENCY: 'USD'
-};
