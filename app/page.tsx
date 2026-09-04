70a055c8583e58df2e3b98f9f04bfb2ed41d4f84
Chriskefas22 <angellhinoc@gmail.com>
2026-09-04 22:44:39 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,16 @@
-import {redirect} from 'next/navigation';export default function Page(){redirect('/dashboard')}
+import { redirect } from 'next/navigation';
+import { createClient } from '@/lib/supabase/server';
+
+export default async function Page() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect('/dashboard');
+  }
+
+  redirect('/auth/login');
+}
