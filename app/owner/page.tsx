36dea8844e5780b06f4513658016f0e206dca828
Chriskefas22 <angellhinoc@gmail.com>
2026-09-04 21:21:29 +0800

    Add OwnerPage component with OwnerControlCenter

---
+++ app/owner/page.tsx
@@ -0,0 +1,7 @@
+import OwnerControlCenter from '@/components/owner/OwnerControlCenter';
+
+export const dynamic = 'force-dynamic';
+
+export default function OwnerPage() {
+  return <OwnerControlCenter />;
+}
